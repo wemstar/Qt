5cdf7d8bb0d5d390fa271ea59fd63a3788624033
wemstar <sylwestermacura@gmail.com>
2013-03-11 18:33:11 +0100

    plik zawiera implementacje gotowej klasy tabelka

--- lab01_2/Tabelka.cpp
+++ lab01_2/Tabelka.cpp
@@ -3,5 +3,54 @@
 
 Tabelka::Tabelka(QWidget* parent):QWidget(parent)
 {
+	textBrowser=new QTextBrowser();
+
+	valueSpin=new QSpinBox();
+
+	valueEdit=new QLineEdit();
+
+	numberCheck=new QCheckBox("Number");
+	textCheck=new QCheckBox("Descpiption");
+
+	addButton=new QPushButton("Add");
+	QObject::connect(addButton,SIGNAL(clicked()),this,SLOT(addText()));
+
+
+	clearButton=new QPushButton("Clear All");
+	QObject::connect(clearButton,SIGNAL(clicked()),textBrowser,SLOT(clear()));
+	quitButton=new QPushButton("Quit");
+	QObject::connect(quitButton,SIGNAL(clicked()),this,SLOT(close()));
+
+	buttonLayout=new QVBoxLayout();
+	buttonLayout->addWidget(numberCheck);
+	buttonLayout->addWidget(valueSpin);
+	buttonLayout->addWidget(textCheck);
+	buttonLayout->addWidget(valueEdit);
+	buttonLayout->addWidget(addButton);
+	buttonLayout->addStretch();
+	buttonLayout->addWidget(clearButton);
+
+	upperLayout=new QHBoxLayout();
+	upperLayout->addWidget(textBrowser);
+	upperLayout->addLayout(buttonLayout);
+
+	mainLayout=new QVBoxLayout();
+	mainLayout->addLayout(upperLayout);
+	mainLayout->addWidget(quitButton);
+	setLayout(mainLayout);
+
+
+
+
+}
+void Tabelka::addText()
+{
+	QString str;
+	if(numberCheck->isChecked())str+=QString("%1").arg(valueSpin->value());
+	if(textCheck->isChecked())str+=QString(valueEdit->text());
+	// str+=QString("\n");
+
+	textBrowser->append(str);
+
 
 }
