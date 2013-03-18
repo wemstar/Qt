4ea3eda26e24f6893e2144f30ce8b4591c1c0d55
wemstar <sylwestermacura@gmail.com>
2013-03-18 16:31:16 +0100

    plik zawiera maina z laboratoriow z 18.03.2013 na jego podstawie trzeba zrobic widgety

---
+++ lab02/main.cpp
@@ -0,0 +1,40 @@
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QWidget>
+#include "Kostka.h"
+#include "Dane.h"
+
+
+//
+//  Funkcji main NIE WOLNO zmieniac!
+//
+int main( int argc, char **argv ){
+    QApplication app( argc, argv );
+    QWidget window;
+
+    Kostka* kostkaA = new Kostka;
+    Kostka* kostkaB = new Kostka;
+    Dane* podsumowanie = new Dane;
+
+    QObject::connect(kostkaA, SIGNAL(wylosowane(QString, int)),
+                     podsumowanie, SLOT(nowy(QString,int)) );
+    QObject::connect(kostkaB, SIGNAL(wylosowane(QString, int)),
+                     podsumowanie, SLOT(nowy(QString,int)) );
+
+
+    QHBoxLayout* layoutKostki = new QHBoxLayout;
+    layoutKostki->addWidget(kostkaA);
+    layoutKostki->addWidget(kostkaB);
+
+    QVBoxLayout* layoutOkno = new QVBoxLayout;
+    layoutOkno->addLayout(layoutKostki);
+    layoutOkno->addWidget(new QLabel("Wyniki losowan :"));
+    layoutOkno->addWidget(podsumowanie);
+
+    window.setLayout(layoutOkno);
+    window.show();
+
+    return app.exec();
+}
