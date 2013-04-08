9c3348aa3c0140a7a94c7f54259613b024903e7e
wemstar <sylwestermacura@gmail.com>
2013-04-08 22:46:58 +0200

    Pliki z moimi Tłumaczeniemi

---
+++ Lab04/kalkulatorpl_PL.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Główne okno</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="27"/>
+        <source>Filename</source>
+        <translation>Nazwa Pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <source>0</source>
+        <translation>O Programie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="87"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <location filename="mainwindow.ui" line="157"/>
+        <source>About</source>
+        <translation>O Programie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="121"/>
+        <source>open</source>
+        <translation>Otworz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="124"/>
+        <source>Open File</source>
+        <translation>Otwórz plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="133"/>
+        <source>save</source>
+        <translation>Zapisz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="136"/>
+        <source>Save File</source>
+        <translation>Zapisz plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="145"/>
+        <source>exit</source>
+        <translation>Wyjśćie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="148"/>
+        <source>Exit the program</source>
+        <translation>Wyjśćie z programu</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="160"/>
+        <source>About the progrma</source>
+        <translation>O Programie</translation>
+    </message>
+</context>
+</TS>
