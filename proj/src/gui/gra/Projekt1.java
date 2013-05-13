f688291a78ca62ac2af1d707d38d1c3391fa5f4d
Wemstar <sylwestermacura@gmail.com>
2013-05-13 18:41:17 +0200

    Nowy projekt z GUI

---
+++ proj/src/gui/gra/Projekt1.java
@@ -0,0 +1,20 @@
+package gui.gra;
+
+public class Projekt1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+	String print() {
+		return "cos";
+		
+		
+	}
+	
+
+}
---
+++ proj/test/gui/gra/Test.java
@@ -0,0 +1,14 @@
+package gui.gra;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		
+		String cos=new Projekt1().print();
+		assertEquals(cos,"cos");
+	}
+
+}
