68dc25e043eb84efc7c9b0a1a08ad8799c63db29
Wemstar <sylwestermacura@gmail.com>
2013-05-21 17:37:15 +0200

    Dadano stocznie która tworzy statki

---
+++ proj/src/gui/gra/statki/StatekFactory.java
@@ -0,0 +1,39 @@
+package gui.gra.statki;
+
+import gui.gra.wyjatki.BadPositionException;
+import gui.gra.wyjatki.LimitException;
+
+import java.awt.Point;
+
+
+
+/**
+ * @author wemstar
+ *klasa buduje staki dla limitu
+ */
+public class StatekFactory {
+	
+	/**
+	 * @param cztery limit dla czteromasztowców
+	 * @param trzy limit dla trójmasztowców
+	 * @param dwa limit dla dwumasztowców
+	 * @param jeden limit dla jednomasztowców
+	 */
+	public StatekFactory(int cztery,int trzy,int dwa,int jeden)
+	{
+		limits[0]=jeden;
+		limits[1]=dwa;
+		limits[2]=trzy;
+		limits[3]=cztery;
+	}
+	
+	public Statek zwodujStetek(Point punkt,int iloscMasztow,int dir) throws BadPositionException, LimitException
+	{
+		if(limits[iloscMasztow-1]<1)throw new LimitException();
+		limits[iloscMasztow-1]--;
+		return new Statek( punkt, iloscMasztow,dir);
+		
+	}
+	private int[] limits={4,3,2,1};
+
+}
